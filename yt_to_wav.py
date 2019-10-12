356271a447d98c7d137708b2bbe82999fcbb97f7
harryahlas <42125399+harryahlas@users.noreply.github.com>
2019-10-11 18:08:19 -0700

    Update yt_to_wav.py

--- yt_to_wav.py
+++ yt_to_wav.py
@@ -15,16 +15,21 @@ import youtube_dl
 #os.mkdir('C:\\Users\\Anyone\\Music\\Enslaved\\')
 #os.mkdir('C:\\Users\\Anyone\\Music\\Enslaved\\E')
 
-os.chdir('C:\\Users\\Anyone\\Music\\Enslaved\\E')
+os.chdir('C:\\Users\\Anyone\\Music\\Possessed\\Revelations of Oblivion')
 
-url_list = ['https://www.youtube.com/watch?v=Y8HX_vGPCz8&list=OLAK5uy_noFAGW-89cd1HrRCnBFYrufZxuB7UUu6g&index=1',
-'https://www.youtube.com/watch?v=Y8HX_vGPCz8&list=OLAK5uy_noFAGW-89cd1HrRCnBFYrufZxuB7UUu6g&index=2',
-'https://www.youtube.com/watch?v=Y8HX_vGPCz8&list=OLAK5uy_noFAGW-89cd1HrRCnBFYrufZxuB7UUu6g&index=3',
-'https://www.youtube.com/watch?v=Y8HX_vGPCz8&list=OLAK5uy_noFAGW-89cd1HrRCnBFYrufZxuB7UUu6g&index=4',
-'https://www.youtube.com/watch?v=Y8HX_vGPCz8&list=OLAK5uy_noFAGW-89cd1HrRCnBFYrufZxuB7UUu6g&index=5',
-'https://www.youtube.com/watch?v=Y8HX_vGPCz8&list=OLAK5uy_noFAGW-89cd1HrRCnBFYrufZxuB7UUu6g&index=6',
-'https://www.youtube.com/watch?v=Y8HX_vGPCz8&list=OLAK5uy_noFAGW-89cd1HrRCnBFYrufZxuB7UUu6g&index=7',
-'https://www.youtube.com/watch?v=Y8HX_vGPCz8&list=OLAK5uy_noFAGW-89cd1HrRCnBFYrufZxuB7UUu6g&index=8']
+url_list = ['https://www.youtube.com/watch?v=tq0y5Vs-heI&list=PLfUV806q_Ri7wcsLO8hjbCEG-jXEBqrzS&index=1',
+'https://www.youtube.com/watch?v=tq0y5Vs-heI&list=PLfUV806q_Ri7wcsLO8hjbCEG-jXEBqrzS&index=2',
+'https://www.youtube.com/watch?v=tq0y5Vs-heI&list=PLfUV806q_Ri7wcsLO8hjbCEG-jXEBqrzS&index=3',
+'https://www.youtube.com/watch?v=tq0y5Vs-heI&list=PLfUV806q_Ri7wcsLO8hjbCEG-jXEBqrzS&index=4',
+'https://www.youtube.com/watch?v=tq0y5Vs-heI&list=PLfUV806q_Ri7wcsLO8hjbCEG-jXEBqrzS&index=5',
+'https://www.youtube.com/watch?v=tq0y5Vs-heI&list=PLfUV806q_Ri7wcsLO8hjbCEG-jXEBqrzS&index=6',
+'https://www.youtube.com/watch?v=tq0y5Vs-heI&list=PLfUV806q_Ri7wcsLO8hjbCEG-jXEBqrzS&index=7',
+'https://www.youtube.com/watch?v=tq0y5Vs-heI&list=PLfUV806q_Ri7wcsLO8hjbCEG-jXEBqrzS&index=8',
+'https://www.youtube.com/watch?v=tq0y5Vs-heI&list=PLfUV806q_Ri7wcsLO8hjbCEG-jXEBqrzS&index=9',
+'https://www.youtube.com/watch?v=tq0y5Vs-heI&list=PLfUV806q_Ri7wcsLO8hjbCEG-jXEBqrzS&index=10',
+'https://www.youtube.com/watch?v=tq0y5Vs-heI&list=PLfUV806q_Ri7wcsLO8hjbCEG-jXEBqrzS&index=11',
+'https://www.youtube.com/watch?v=tq0y5Vs-heI&list=PLfUV806q_Ri7wcsLO8hjbCEG-jXEBqrzS&index=12'
+]
 
 def download_youtube_music(url_address):
     ydl_opts = {
@@ -41,56 +46,5 @@ def download_youtube_music(url_address):
         
 for url in url_list:
     download_youtube_music(url) 
-    
-    
-#### Automata I
-os.mkdir('C:\\Users\\Anyone\\Music\\Between the Buried and Me\\')
-os.mkdir('C:\\Users\\Anyone\\Music\\Between the Buried and Me\\Automata I')
-
-os.chdir('C:\\Users\\Anyone\\Music\\Between the Buried and Me\\Automata I')
-
-url_list = ['https://www.youtube.com/watch?v=kPU9YTN-faQ&list=OLAK5uy_nujrpDLn0uRH01q9YEHfFVt4IQIoWD4u8&index=1',
-'https://www.youtube.com/watch?v=kPU9YTN-faQ&list=OLAK5uy_nujrpDLn0uRH01q9YEHfFVt4IQIoWD4u8&index=2',
-'https://www.youtube.com/watch?v=kPU9YTN-faQ&list=OLAK5uy_nujrpDLn0uRH01q9YEHfFVt4IQIoWD4u8&index=3',
-'https://www.youtube.com/watch?v=kPU9YTN-faQ&list=OLAK5uy_nujrpDLn0uRH01q9YEHfFVt4IQIoWD4u8&index=4',
-'https://www.youtube.com/watch?v=kPU9YTN-faQ&list=OLAK5uy_nujrpDLn0uRH01q9YEHfFVt4IQIoWD4u8&index=5',
-'https://www.youtube.com/watch?v=kPU9YTN-faQ&list=OLAK5uy_nujrpDLn0uRH01q9YEHfFVt4IQIoWD4u8&index=6']
-      
-for url in url_list:
-    download_youtube_music(url)     
-    
-    
-#### Automata II
-#os.mkdir('C:\\Users\\Anyone\\Music\\Between the Buried and Me\\')
-os.mkdir('C:\\Users\\Anyone\\Music\\Between the Buried and Me\\Automata II')
-os.chdir('C:\\Users\\Anyone\\Music\\Between the Buried and Me\\Automata II')
-
-url_list = ['https://www.youtube.com/watch?v=7W8u1IFK7Y8&list=PLHJQ__I7nFk9XJ-9p4LVWrG4BcCqyL0Ig&index=7',
-'https://www.youtube.com/watch?v=7W8u1IFK7Y8&list=PLHJQ__I7nFk9XJ-9p4LVWrG4BcCqyL0Ig&index=8',
-'https://www.youtube.com/watch?v=7W8u1IFK7Y8&list=PLHJQ__I7nFk9XJ-9p4LVWrG4BcCqyL0Ig&index=9',
-'https://www.youtube.com/watch?v=7W8u1IFK7Y8&list=PLHJQ__I7nFk9XJ-9p4LVWrG4BcCqyL0Ig&index=10']
-      
-for url in url_list:
-    download_youtube_music(url)     
-    
-   
-#### Exhorder Topic
-os.mkdir('C:\\Users\\Anyone\\Music\\Exhorder')
-os.mkdir('C:\\Users\\Anyone\\Music\\Exhorder\\Topic')
-os.chdir('C:\\Users\\Anyone\\Music\\Exhorder\\Topic')
-
-url_list = ['https://www.youtube.com/watch?v=QezU673nHhk&list=OLAK5uy_l7fA0T2vfnBu6j3B5WO0tAjAwS7fparcM&index=1',
-'https://www.youtube.com/watch?v=QezU673nHhk&list=OLAK5uy_l7fA0T2vfnBu6j3B5WO0tAjAwS7fparcM&index=1',
-'https://www.youtube.com/watch?v=QezU673nHhk&list=OLAK5uy_l7fA0T2vfnBu6j3B5WO0tAjAwS7fparcM&index=1',
-'https://www.youtube.com/watch?v=QezU673nHhk&list=OLAK5uy_l7fA0T2vfnBu6j3B5WO0tAjAwS7fparcM&index=1',
-'https://www.youtube.com/watch?v=QezU673nHhk&list=OLAK5uy_l7fA0T2vfnBu6j3B5WO0tAjAwS7fparcM&index=1',
-'https://www.youtube.com/watch?v=QezU673nHhk&list=OLAK5uy_l7fA0T2vfnBu6j3B5WO0tAjAwS7fparcM&index=1',
-'https://www.youtube.com/watch?v=QezU673nHhk&list=OLAK5uy_l7fA0T2vfnBu6j3B5WO0tAjAwS7fparcM&index=1',
-'https://www.youtube.com/watch?v=QezU673nHhk&list=OLAK5uy_l7fA0T2vfnBu6j3B5WO0tAjAwS7fparcM&index=1',
-'https://www.youtube.com/watch?v=QezU673nHhk&list=OLAK5uy_l7fA0T2vfnBu6j3B5WO0tAjAwS7fparcM&index=1',
-'https://www.youtube.com/watch?v=QezU673nHhk&list=OLAK5uy_l7fA0T2vfnBu6j3B5WO0tAjAwS7fparcM&index=1']
-      
-for url in url_list:
-    download_youtube_music(url)     
     
     

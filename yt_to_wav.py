545b18e463b4f68f8564e3e1dbd3a1fa05db4807
harryahlas <42125399+harryahlas@users.noreply.github.com>
2019-11-10 19:10:54 -0800

    Update yt_to_wav.py

--- yt_to_wav.py
+++ yt_to_wav.py
@@ -12,25 +12,39 @@ from __future__ import unicode_literals
 import os
 import youtube_dl
 
-os.mkdir('C:\\Users\\Anyone\\Music\\My Little Pony\\Movie Soundtrack')
+os.mkdir('C:\\Users\\Anyone\\Music\\Blind Guardian')
+os.mkdir('C:\\Users\\Anyone\\Music\\Blind Guardian\\Legacy of the Dark Lands')
 #os.mkdir('C:\\Users\\Anyone\\Music\\Enslaved\\E')
 
-os.chdir('C:\\Users\\Anyone\\Music\\My Little Pony\\Movie Soundtrack')
+os.chdir('C:\\Users\\Anyone\\Music\\Blind Guardian\\Legacy of the Dark Lands')
 
-url_list = ['https://www.youtube.com/watch?v=BsS0bD83y1A&list=PLfc6_O5ggVzJkPKlM7AU0hye5tE-UMRy9',
-'https://www.youtube.com/watch?v=ueU7NmVC3JQ&list=PLfc6_O5ggVzJkPKlM7AU0hye5tE-UMRy9&index=2',
-'https://www.youtube.com/watch?v=ueU7NmVC3JQ&list=PLfc6_O5ggVzJkPKlM7AU0hye5tE-UMRy9&index=3',
-'https://www.youtube.com/watch?v=ueU7NmVC3JQ&list=PLfc6_O5ggVzJkPKlM7AU0hye5tE-UMRy9&index=4',
-'https://www.youtube.com/watch?v=ueU7NmVC3JQ&list=PLfc6_O5ggVzJkPKlM7AU0hye5tE-UMRy9&index=5',
-'https://www.youtube.com/watch?v=ueU7NmVC3JQ&list=PLfc6_O5ggVzJkPKlM7AU0hye5tE-UMRy9&index=6',
-'https://www.youtube.com/watch?v=ueU7NmVC3JQ&list=PLfc6_O5ggVzJkPKlM7AU0hye5tE-UMRy9&index=7',
-'https://www.youtube.com/watch?v=ueU7NmVC3JQ&list=PLfc6_O5ggVzJkPKlM7AU0hye5tE-UMRy9&index=8',
-'https://www.youtube.com/watch?v=ueU7NmVC3JQ&list=PLfc6_O5ggVzJkPKlM7AU0hye5tE-UMRy9&index=9',
-'https://www.youtube.com/watch?v=ueU7NmVC3JQ&list=PLfc6_O5ggVzJkPKlM7AU0hye5tE-UMRy9&index=10',
-'https://www.youtube.com/watch?v=ueU7NmVC3JQ&list=PLfc6_O5ggVzJkPKlM7AU0hye5tE-UMRy9&index=11',
-'https://www.youtube.com/watch?v=ueU7NmVC3JQ&list=PLfc6_O5ggVzJkPKlM7AU0hye5tE-UMRy9&index=12',
-'https://www.youtube.com/watch?v=ueU7NmVC3JQ&list=PLfc6_O5ggVzJkPKlM7AU0hye5tE-UMRy9&index=13',
-'https://www.youtube.com/watch?v=ueU7NmVC3JQ&list=PLfc6_O5ggVzJkPKlM7AU0hye5tE-UMRy9&index=14']
+url_list = ['https://www.youtube.com/watch?v=MnRT2uukgZk&list=PLddSkUxmPEC9pIdW125q0Ivv5d2p6uzmD&index=1',
+'https://www.youtube.com/watch?v=MnRT2uukgZk&list=PLddSkUxmPEC9pIdW125q0Ivv5d2p6uzmD&index=2',
+'https://www.youtube.com/watch?v=MnRT2uukgZk&list=PLddSkUxmPEC9pIdW125q0Ivv5d2p6uzmD&index=3',
+'https://www.youtube.com/watch?v=MnRT2uukgZk&list=PLddSkUxmPEC9pIdW125q0Ivv5d2p6uzmD&index=4',
+'https://www.youtube.com/watch?v=MnRT2uukgZk&list=PLddSkUxmPEC9pIdW125q0Ivv5d2p6uzmD&index=5',
+'https://www.youtube.com/watch?v=MnRT2uukgZk&list=PLddSkUxmPEC9pIdW125q0Ivv5d2p6uzmD&index=6',
+'https://www.youtube.com/watch?v=MnRT2uukgZk&list=PLddSkUxmPEC9pIdW125q0Ivv5d2p6uzmD&index=7',
+'https://www.youtube.com/watch?v=MnRT2uukgZk&list=PLddSkUxmPEC9pIdW125q0Ivv5d2p6uzmD&index=8',
+'https://www.youtube.com/watch?v=MnRT2uukgZk&list=PLddSkUxmPEC9pIdW125q0Ivv5d2p6uzmD&index=9',
+'https://www.youtube.com/watch?v=MnRT2uukgZk&list=PLddSkUxmPEC9pIdW125q0Ivv5d2p6uzmD&index=10',
+'https://www.youtube.com/watch?v=MnRT2uukgZk&list=PLddSkUxmPEC9pIdW125q0Ivv5d2p6uzmD&index=11',
+'https://www.youtube.com/watch?v=MnRT2uukgZk&list=PLddSkUxmPEC9pIdW125q0Ivv5d2p6uzmD&index=12',
+'https://www.youtube.com/watch?v=MnRT2uukgZk&list=PLddSkUxmPEC9pIdW125q0Ivv5d2p6uzmD&index=13',
+'https://www.youtube.com/watch?v=MnRT2uukgZk&list=PLddSkUxmPEC9pIdW125q0Ivv5d2p6uzmD&index=14',
+'https://www.youtube.com/watch?v=MnRT2uukgZk&list=PLddSkUxmPEC9pIdW125q0Ivv5d2p6uzmD&index=15',
+'https://www.youtube.com/watch?v=MnRT2uukgZk&list=PLddSkUxmPEC9pIdW125q0Ivv5d2p6uzmD&index=16',
+'https://www.youtube.com/watch?v=MnRT2uukgZk&list=PLddSkUxmPEC9pIdW125q0Ivv5d2p6uzmD&index=17',
+'https://www.youtube.com/watch?v=MnRT2uukgZk&list=PLddSkUxmPEC9pIdW125q0Ivv5d2p6uzmD&index=18',
+'https://www.youtube.com/watch?v=MnRT2uukgZk&list=PLddSkUxmPEC9pIdW125q0Ivv5d2p6uzmD&index=19',
+'https://www.youtube.com/watch?v=MnRT2uukgZk&list=PLddSkUxmPEC9pIdW125q0Ivv5d2p6uzmD&index=20',
+'https://www.youtube.com/watch?v=MnRT2uukgZk&list=PLddSkUxmPEC9pIdW125q0Ivv5d2p6uzmD&index=21',
+'https://www.youtube.com/watch?v=MnRT2uukgZk&list=PLddSkUxmPEC9pIdW125q0Ivv5d2p6uzmD&index=22',
+'https://www.youtube.com/watch?v=MnRT2uukgZk&list=PLddSkUxmPEC9pIdW125q0Ivv5d2p6uzmD&index=23',
+'https://www.youtube.com/watch?v=MnRT2uukgZk&list=PLddSkUxmPEC9pIdW125q0Ivv5d2p6uzmD&index=24',
+'https://www.youtube.com/watch?v=MnRT2uukgZk&list=PLddSkUxmPEC9pIdW125q0Ivv5d2p6uzmD&index=25',
+'https://www.youtube.com/watch?v=MnRT2uukgZk&list=PLddSkUxmPEC9pIdW125q0Ivv5d2p6uzmD&index=26',
+'https://www.youtube.com/watch?v=MnRT2uukgZk&list=PLddSkUxmPEC9pIdW125q0Ivv5d2p6uzmD&index=27']
 
 
 def download_youtube_music(url_address):

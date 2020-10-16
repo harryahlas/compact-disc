6013fca622e53673fb6db1372f101054d7feee49
harryahlas <42125399+harryahlas@users.noreply.github.com>
2020-10-15 22:02:59 -0700

    Update yt_to_wav.py

--- yt_to_wav.py
+++ yt_to_wav.py
@@ -11,18 +11,31 @@ Requires youtube_dl as well as ffmpeg
 from __future__ import unicode_literals
 import os
 import youtube_dl
+import json
 
-os.mkdir('C:\\Users\\Anyone\\Music\\My Little Pony\\cd3')
+os.mkdir('C:\\Users\\Anyone\\Music\\Rodrigo y Gabriela\\yt1')
 
-os.chdir('C:\\Users\\Anyone\\Music\\My Little Pony\\cd3')
+os.chdir('C:\\Users\\Anyone\\Music\\Rodrigo y Gabriela\\yt1')
+
+
+
+
+
+def paste0(string1, range1 ):
+#    texts = [string1 + str(num1)  for num1 in zip(range1)]
+    texts = [string1 + str(num1)  for num1 in (range1)]
+    return texts
+
+url_list = paste0("https://www.youtube.com/watch?v=b3lCjBjexWQ&list=PLpcGjv9eMpOPH2bbR1t6GnhQXdaHignQA&index=", 
+               range(1, 43))
 
 url_list = [
 'https://www.youtube.com/watch?v=oAemYgwNCBo',
-'https://www.youtube.com/watch?v=fphUZV7dKUw',
+'https://www.youtube.com/watch?v=fphUZV7dKUw', #doesn't work
 'https://www.youtube.com/watch?v=xthcbwng0Uo',
-'https://www.youtube.com/watch?v=hAXX_bPxIzY&t=65s',
+'https://www.youtube.com/watch?v=hAXX_bPxIzY&t=65s', #doesn't work
 'https://www.youtube.com/watch?v=utyk5gHOAU0',
-'https://www.youtube.com/watch?v=hZUew-9HPI8',
+'https://www.youtube.com/watch?v=hZUew-9HPI8', #doesn't work
 'https://www.youtube.com/watch?v=-bboX3LpWpE',
 'https://www.youtube.com/watch?v=tao1Ic8qVkM',
 'https://www.youtube.com/watch?v=dkTPatXRyh0',
@@ -43,6 +56,7 @@ url_list = [
 
 def download_youtube_music(url_address):
     ydl_opts = {
+        'noplaylist':True,
         'format': 'bestaudio/best',
         'postprocessors': [{
             'key': 'FFmpegExtractAudio',
@@ -53,8 +67,14 @@ def download_youtube_music(url_address):
     }
     with youtube_dl.YoutubeDL(ydl_opts) as ydl:
         ydl.download([url_address])
-        
+
+
+     
+download_youtube_music(url_list[1]) 
+download_youtube_music('https://www.youtube.com/watch?v=b3lCjBjexWQ&list=PLpcGjv9eMpOPH2bbR1t6GnhQXdaHignQA')
+            
+      
 for url in url_list:
     download_youtube_music(url) 
     
-    
+    #download_youtube_music(url_list[3])
